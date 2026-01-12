f5c1779e3e790e3728e369e83028931039614d37
Michael Monsour <michael.monsour@floutsoftware.com>
2026-01-12 15:56:42 +1000

    Udpated tests and build script actions

--- tests/Xtra.Extensions.DependencyInjection.Tests/ServiceCollectionTests.cs
+++ tests/Xtra.Extensions.DependencyInjection.Tests/ServiceCollectionTests.cs
@@ -67,6 +67,23 @@ public class ServiceCollectionTests
 
 
     [Fact]
+    public void ServiceCollection_AddFactory_NullCheck()
+    {
+        var sc = new ServiceCollection();
+       
+        // Test services parameter is null
+        Assert.Throws<ArgumentNullException>(() => ((IServiceCollection)null!).AddFactory<FooService>());
+        Assert.Throws<ArgumentNullException>(() => ((IServiceCollection)null!).AddFactory<ITestService, FooService>());
+        Assert.Throws<ArgumentNullException>(() => ((IServiceCollection)null!).AddFactory<FooService>(() => new FooService()));
+        Assert.Throws<ArgumentNullException>(() => ((IServiceCollection)null!).AddFactory<FooService>(sp => new FooService()));
+
+        // Test other parameters are null
+        Assert.Throws<ArgumentNullException>(() => sc.AddFactory<FooService>((Func<FooService>)null!));
+        Assert.Throws<ArgumentNullException>(() => sc.AddFactory<FooService>((Func<IServiceProvider, FooService>)null!));
+    }
+
+
+    [Fact]
     public void ServiceCollection_AddServiceBundle_WithGenerics()
     {
         var sc = new ServiceCollection();
@@ -116,17 +133,17 @@ public class ServiceCollectionTests
     public void ServiceCollection_AddServiceBundles_NullCheck()
     {
         var sc = new ServiceCollection();
+
+        // Test services parameter is null
+        Assert.Throws<ArgumentNullException>(() => ((IServiceCollection)null!).AddServiceBundle<FooBundle>());
+        Assert.Throws<ArgumentNullException>(() => ((IServiceCollection)null!).AddServiceBundle(new FooBundle()));
+        Assert.Throws<ArgumentNullException>(() => ((IServiceCollection)null!).AddServiceBundles(new FooBundle()));
+        Assert.Throws<ArgumentNullException>(() => ((IServiceCollection)null!).AddServiceBundles(new List<IServiceBundle> { new FooBundle() }));
+
+        // Test other parameters are null
+        Assert.Throws<ArgumentNullException>(() => sc.AddServiceBundle((IServiceBundle)null!));
         Assert.Throws<ArgumentNullException>(() => sc.AddServiceBundles((IServiceBundle[])null!));
         Assert.Throws<ArgumentNullException>(() => sc.AddServiceBundles((IEnumerable<IServiceBundle>)null!));
-    }
-
-
-    [Fact]
-    public void ServiceCollection_AddFactory_NullCheck()
-    {
-        var sc = new ServiceCollection();
-        Assert.Throws<ArgumentNullException>(() => sc.AddFactory<FooService>((Func<FooService>)null!));
-        Assert.Throws<ArgumentNullException>(() => sc.AddFactory<FooService>((Func<IServiceProvider, FooService>)null!));
     }
 
 
